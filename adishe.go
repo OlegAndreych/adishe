833c89a9e5fb7f84d0e9db0e8f06aced7dec7447
Oleg Andreych <kjiec4@gmail.com>
2017-09-20 21:59:00 +0500

    Setting domain restrictions via address-list in /ip/firewall.

--- adishe.go
+++ adishe.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/Netwurx/routeros-api-go"
+	"gopkg.in/routeros.v2"
 
 	"bufio"
 	"net/http"
@@ -15,8 +15,6 @@ import (
 	"github.com/tmc/scp"
 	"golang.org/x/crypto/ssh"
 
-	"path"
-
 	"flag"
 	"strconv"
 
@@ -25,7 +23,6 @@ import (
 
 var log = logging.MustGetLogger("adishe")
 var address = flag.String("addr", "192.168.0.1", "router's address;")
-var sshPort = flag.Int("sp", 22, "ssh port;")
 var apiPort = flag.Int("ap", 8728, "api port;")
 var login = flag.String("l", "", "login;")
 var password = flag.String("p", "", "password;")
@@ -64,26 +61,11 @@ func main() {
 		delChan <- true
 	}
 
-	if setToAdd.Cardinality() > 0 {
-
-		filenameChan := make(chan string, 1)
-		go createScriptFile(filenameChan, setToAdd)
-		sshSessionChan := make(chan *ssh.Session, 1)
-		go createSSHSession(sshSessionChan)
-		scriptFilename := <-filenameChan
-		session := <-sshSessionChan
-		defer session.Close()
-		uploadChan := make(chan bool, 1)
-		scriptFilenameBase := path.Base(scriptFilename)
-		go uploadScript(scriptFilename, scriptFilenameBase, session, uploadChan)
-		<-delChan
-		<-uploadChan
-
-		importScript(client, scriptFilenameBase)
-		removeScriptFile(client, scriptFilenameBase)
-	} else {
-		log.Info("Nothing to do here. Exiting now.")
+	for _, address := range setToAdd.ToSlice() {
+		client.Run("/ip/firewall/address-list/add", fmt.Sprintf("=address=%v", address), "=list=adishe")
 	}
+
+	log.Info("All records have been imported.")
 }
 
 //Prepares log facility.
@@ -98,12 +80,7 @@ func prepareLog() {
 // Establishes connection with Mikrotik router.
 func connectToMtk() *routeros.Client {
 	log.Info("Connecting to Mikrotik.")
-	client, err := routeros.New(*address + ":" + strconv.Itoa(*apiPort))
-	if err != nil {
-		log.Critical(err)
-		panic(err)
-	}
-	err = client.Connect(*login, *password)
+	client, err := routeros.Dial(*address+":"+strconv.Itoa(*apiPort), *login, *password)
 	if err != nil {
 		log.Critical(err)
 		panic(err)
@@ -115,17 +92,16 @@ func connectToMtk() *routeros.Client {
 // Retrieves router's static DNS table.
 func retrieveRouterData(ch chan map[string]string, client *routeros.Client) {
 	log.Info("Retrieving router's static DNS entries.")
-	args := []routeros.Pair{*routeros.NewPair("comment", "adishe")}
-	params := []string{".id", "name"}
-	res, err := client.Query("/ip/dns/static/print", routeros.Query{Op: "=", Pairs: args, Proplist: params})
+	res, err := client.Run("/ip/firewall/address-list/print", "?dynamic=false", "=.proplist=.id,address")
 	if err != nil {
 		log.Critical(err)
 		panic(err)
 	}
 
 	routerMap := make(map[string]string)
-	for _, val := range res.SubPairs {
-		routerMap[val["name"]] = val[".id"]
+	for _, val := range res.Re {
+		vMap := val.Map
+		routerMap[vMap["name"]] = vMap[".id"]
 	}
 
 	ch <- routerMap
@@ -174,8 +150,7 @@ func removeObsoleteRecords(delChan chan bool, setToDeletePtr *mapset.Set, router
 		for i, val := range setToDelete.ToSlice() {
 			idsToDelete[i] = routerMap[val.(string)]
 		}
-		params := []routeros.Pair{routeros.Pair{Op: "=", Key: "numbers", Value: strings.Join(idsToDelete, ",")}}
-		res, err := client.Call("/ip/dns/static/remove", params)
+		res, err := client.Run("/ip/firewall/address-list/remove", "?numbers="+strings.Join(idsToDelete, ","))
 		if err != nil {
 			log.Critical(err)
 			panic(err)
@@ -208,26 +183,6 @@ func createScriptFile(tempFilenameChan chan string, setToAdd mapset.Set) {
 	log.Info("Obsolete records has been removed from router.")
 }
 
-// Establishes SSH session for script uploading.
-func createSSHSession(sshSessionChan chan *ssh.Session) {
-	log.Info("Creating ssh session.")
-	sshClient, err := ssh.Dial("tcp", *address+":"+strconv.Itoa(*sshPort), &ssh.ClientConfig{
-		User: *login,
-		Auth: []ssh.AuthMethod{
-			ssh.Password(*password),
-		},
-	})
-
-	session, err := sshClient.NewSession()
-	if err != nil {
-		log.Critical(err)
-		panic(err)
-	}
-
-	sshSessionChan <- session
-	log.Info("Ssh session has been created.")
-}
-
 // Uploads script via established ssh session.
 func uploadScript(scriptFilename string, scriptFilenameBase string, session *ssh.Session, uploadChan chan bool) {
 	log.Info("Uploading script.")
@@ -238,26 +193,4 @@ func uploadScript(scriptFilename string, scriptFilenameBase string, session *ssh
 	}
 	uploadChan <- true
 	log.Info("Script has been uploaded.")
-}
-
-// Applies uploaded script.
-func importScript(client *routeros.Client, scriptFilenameBase string) {
-	log.Info("Importing script.")
-	_, err := client.Call(fmt.Sprintf("/import"), []routeros.Pair{*routeros.NewPair("file-name", scriptFilenameBase)})
-	if err != nil {
-		log.Critical(err)
-		panic(err)
-	}
-	log.Info("Script has been imported.")
-}
-
-// Removes applied script.
-func removeScriptFile(client *routeros.Client, scriptFilenameBase string) {
-	log.Info("Removing script.")
-	_, err := client.Call(fmt.Sprintf("/file/remove"), []routeros.Pair{*routeros.NewPair("numbers", scriptFilenameBase)})
-	if err != nil {
-		log.Critical(err)
-		panic(err)
-	}
-	log.Info("Script has been removed.")
 }
